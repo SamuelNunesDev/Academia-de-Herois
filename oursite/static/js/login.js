d0f9f2ecc17be44c894af65b21e091431fad1d7c
SamuelNunesDev <sammoc11@hotmail.com>
2021-07-09 12:12:37 -0300

    validação de senhas e troca dinâmica de forms

--- oursite/static/js/login.js
+++ oursite/static/js/login.js
@@ -1,12 +1,26 @@
 function login()
 {
+    let inputPassowrd1 = document.querySelector('#InputPassword1')
+    let InputPassword2 = document.querySelector('#InputPassword2')
+    let form = document.querySelector('#formulario')
+
+    if (inputPassowrd1.value === InputPassword2.value)
+    {
+        form.submit()
+    } 
+}
+
+let msg = document.querySelector('#msg')
+
+if (msg.innerHTML == 'Sucesso! Usuário cadastrado com sucesso.')
+{
     let inputName = document.querySelector('#inputName')
-    let senha = document.querySelector('#inputSenha')
+    let confimPass = document.querySelector('#inputSenha')
     let titulo = document.querySelector('#loginTitle')
     let btCadastro = document.querySelector('#btCadastro')
-    
+
     inputName.parentNode.removeChild(inputName)
-    senha.parentNode.removeChild(senha)
+    confimPass.parentNode.removeChild(confimPass)
     btCadastro.classList.remove('btn-primary')
     btCadastro.classList.add('btn-success')
     titulo.innerHTML = 'Entre Agora!'
