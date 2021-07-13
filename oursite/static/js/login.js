e7071cf40aa2cc509e00e8ab763a3579536c6aa9
SamuelNunesDev <sammoc11@hotmail.com>
2021-07-12 23:18:31 -0300

    sistema de navagação incluído no index

--- oursite/static/js/login.js
+++ oursite/static/js/login.js
@@ -1,7 +1,13 @@
-function login()
+let inputName = document.querySelector('#inputName')
+let confimPass = document.querySelector('#inputSenha')
+let titulo = document.querySelector('#loginTitle')
+let btCadastro = document.querySelector('#btCadastro')
+let btEntrar = document.querySelector('#entrar')
+let inputPassword1 = document.querySelector('#InputPassword1')
+let inputPassword2 = document.querySelector('#InputPassword2')
+
+function autenticar()
 {
-    let inputPassword1 = document.querySelector('#InputPassword1')
-    let inputPassword2 = document.querySelector('#InputPassword2')
     let form = document.querySelector('#formulario')
 
     if (inputPassword1.value != inputPassword2.value)
@@ -10,25 +16,28 @@ function login()
         inputPassword2.value = ''
         alert('As senhas não conferem!')
     }
-    else if (inputPassword1.value != '')
-    {
-        form.submit()
-    }
-    else
+    else if (inputPassword1.value == '')
     {
         alert('Não é permitido cadastrar uma senha vazia! Preencha os campos de tente novamente!')
     }
 }
-/*
-let inputName = document.querySelector('#inputName')
-let confimPass = document.querySelector('#inputSenha')
-let titulo = document.querySelector('#loginTitle')
-let btCadastro = document.querySelector('#btCadastro')
+function login()
+{
+    inputName.parentNode.removeChild(inputName)
+    confimPass.parentNode.removeChild(confimPass)
+    btCadastro.classList.remove('btn-primary')
+    btCadastro.classList.add('btn-success')
+    btCadastro.innerHTML = 'Entrar'
 
-inputName.parentNode.removeChild(inputName)
-confimPass.parentNode.removeChild(confimPass)
-btCadastro.classList.remove('btn-primary')
-btCadastro.classList.add('btn-success')
-titulo.innerHTML = 'Entre Agora!'
-btCadastro.innerHTML = 'Entrar'
-*/
+    titulo.innerHTML = 'Entre Agora!'
+
+    btEntrar.classList.remove('btn-success')
+    btEntrar.classList.add('btn-primary')
+    btEntrar.innerHTML = 'Cadastre-se'
+    btEntrar.setAttribute('onclick', 'cadastro()')
+}
+function cadastro()
+{
+    let index = document.querySelector('#index')
+    index.href = 'file:///C:/Users/Dell%20i5/Documents/Git/projeto-estudos/views/index.html'
+}
